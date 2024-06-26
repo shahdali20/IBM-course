8cc7499d1acef50d108a3b7055bb1c956552ced3
shahdali20 <shahadali.123987@gmail.com>
2024-06-26 22:04:08 +0300

    new commit changes

--- javascript/scripts/script.js
+++ javascript/scripts/script.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 ,specialization:'Javascript'},
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 ,specialization:'Python'},
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 ,specialization:'Java'},
 ]
 
 function displayEmployees() {
@@ -29,5 +29,15 @@ function findEmployeeById(employeeId) {
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
 
+    }
+}
+
+function findSpecialization(special) {
+    const foundemployee = employees.find(employee => employee.specialization === 'Javascript');
+    if (foundemployee) {
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundemployee.id}: ${foundemployee.name}: ${foundemployee.name} - ${foundemployee.department} - $${foundemployee.salary} - ${foundemployee.specialization}</p>`;
+    }
+    else {
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }
 }
